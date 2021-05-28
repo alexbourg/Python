9ca2c4f91a1bf39dea278db40de86cecb765ade4
Alex Bourg <51832082+alexbourg@users.noreply.github.com>
2021-05-28 19:59:10 +0200

    enveditor

---
+++ Tutorials/Env_var/meta.py
@@ -0,0 +1,15 @@
+import pyinstaller_versionfile
+
+pyinstaller_versionfile.create_versionfile(
+    output_file="metadata.txt",
+    version="0.0.1",
+    company_name="CACIB Application Packaging",
+    file_description="Environment variables editor",
+    internal_name="Environment variables editor",
+    legal_copyright="Copyright © 2021 CACIB Application Packaging. All rights reserved.",
+    original_filename="enveditor.exe",
+    product_name="Environment variables editor"
+)
+
+
+#--version-file="D:\GitHub\Python\Tutorials\Env_var\metadata.txt"
